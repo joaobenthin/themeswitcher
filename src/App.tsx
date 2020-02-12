7be728d0999460663ebfcfce04e0caeacfaaf76f
joaobenthin <joaopaulobenthin@hotmail.com>
2020-02-11 21:17:59 -0300

    feat(theme switcher): done

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import usePersistedState from './utils/usePersistedState';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light'? dark : light)
+  }
+
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header toggleTheme={toggleTheme} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#7159c1',
+    secundary: '#222',
+
+    background: '#222',
+    text: '#fff',
+  },
+}
